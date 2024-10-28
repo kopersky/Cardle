fd3c0e76084e91be8f9abbf16358940d720b4bf7
baqumIT <bartek060106@outlook.com>
2024-10-28 23:20:56 +0100

    proba wyswietlania od gory

--- Classic/classic.js
+++ Classic/classic.js
@@ -1,46 +1,41 @@
-
-function func(){
+function func() {
     var x = document.getElementById("guess").value;
     var carBrand = 'bmw';
 
-    if (x.toLowerCase() == carBrand){
-        var div = document.createElement("div");
-        div.style.display = "flex";
-        div.style.margin = "0 auto";
-        div.style.height = "200px";
-        div.style.width = "200px";
-        div.style.backgroundColor = "green";
-        div.style.marginTop = "15px";
-        div.textContent = x;
-        div.style.justifyContent = "center";
-        div.style.alignItems = "center"
-
-        document.body.appendChild(div);
-        
+    if (x.toLowerCase() === carBrand) {
+        var div = createResultDiv(x, "green");
+        addResultToTop(div);
         setTimeout(() => {
             window.location.reload();
-          }, 1000);
+        }, 1000);
+    } 
+    else if (x === "") {
+        return alert("Pole nie może być puste.");
+    } 
+    else {
+        var div = createResultDiv(x, "red");
+        addResultToTop(div);
     }
-    else if (x === ""){
-        return alert(null);
-    }
-    else{
-        var div = document.createElement("div");
-        div.style.margin = "0 auto";
-        div.style.display = "flex";
-        div.style.height = "200px";
-        div.style.width = "200px";
-        div.style.backgroundColor = "red";
-        div.style.marginTop = "15px";
-        div.textContent = x;
-        div.style.justifyContent = "center";
-        div.style.alignItems = "center";
-        document.body.appendChild(div);
-    }
-
 }
 
+function createResultDiv(text, bgColor) {
+    var div = document.createElement("div");
+    div.style.display = "flex";
+    div.style.height = "200px";
+    div.style.width = "200px";
+    div.style.backgroundColor = bgColor;
+    div.style.marginTop = "15px";
+    div.style.justifyContent = "center";
+    div.style.alignItems = "center";
+    div.textContent = text;
+    return div;
+}
 
 function clearInput() {
     document.getElementById('guess').value = '';
+}
+
+function addResultToTop(div) {
+    const resultsContainer = document.getElementById("results");
+    resultsContainer.prepend(div);
 }
