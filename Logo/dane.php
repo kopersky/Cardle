a53bf2ae1e5d03dd807d3ac1c11dc8c24b7fd255
Nikenne <mrkawa8909@gmail.com>
2024-11-04 18:41:38 +0100

    kaka

---
+++ Logo/dane.php
@@ -0,0 +1,27 @@
+<?php
+echo 'PHP dziala ';
+$servername = "localhost"; 
+$username = "root";        
+$password = "";            
+$dbname = "cardle";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Chuj nie dziala: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, logo FROM cardle";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"] . " - logo: " . $row["logo"];
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+
+?>
