9dfd2af68c716b20009dbd6203142591fab8d6b2
Nikenne <mrkawa8909@gmail.com>
2024-11-06 15:48:44 +0100

    php nie phpuje

--- Classic/dane_classic.php
+++ Classic/dane_classic.php
@@ -10,12 +10,12 @@ if ($conn->connect_error) {
     die("Chuj nie dziala: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, logo FROM cardle WHERE id=1";
+$sql = "SELECT id, marka, rok_produkcji, model, typ_nadwozia FROM cardle WHERE id=1" ;
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo "ID: " . $row["id"] . " - logo: " . $row["logo"];
+        echo "ID: " . $row["id"] . " - marka: " . $row["marka"] . " - rok produkcji: " . $row["rok_produkcji"] . " - model: " . $row["model"] . " - typ nadwozia: " . $row["typ_nadwozia"];
     }
 } else {
     echo "0 results";
