50078cdb2d0071a3b8b9941947deec64b6b1b7d9
baqumIT <bartek060106@outlook.com>
2025-03-06 16:47:41 +0100

    troszku css

--- Classic/classic.php
+++ Classic/classic.php
@@ -8,19 +8,20 @@
     <title>Classic</title>
     <script src="classic.js"></script>
 </head>
-<body><center>
-    <div class="logo">
-        <a href="../main.php">
-            <img src="../Graphics/logo.png">
-        </a>
-    </div></center>
-    <form onsubmit="clearInput(); return false;" method = "POST">
-        <input type="text" id="guess" value="" placeholder="Podaj nazwę samochodu" onkeydown="checkEnter()"></input>
-        <button type="submit" id="but" onclick="func()" name = "sub">Submit</button>
-    </form>
+<body>
     <center>
-    <div id="results" class="results"></div>
-    <div id="car-info"></div>
+        <div class="logo">
+            <a href="../main.php">
+                <img src="../Graphics/logo.png">
+            </a>
+        </div></center>
+        <form onsubmit="clearInput(); return false;" method = "POST" class="input">
+            <input type="text" id="guess" value="" placeholder="Podaj nazwę samochodu" onkeydown="checkEnter()"></input>
+            <button type="submit" id="but" onclick="func()" name = "sub">Submit</button>
+        </form>
+        <center>
+        <div id="results" class="results"></div>
+        <div id="car-info"></div>
     </center>
 </body>
 </html>
