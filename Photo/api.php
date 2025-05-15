3b8151383970189a21246d5ff4ad81ffe9508f83
baqumIT <bartek060106@outlook.com>
2025-05-15 12:09:56 +0200

    losowa z bazy

--- Photo/api.php
+++ Photo/api.php
@@ -9,7 +9,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die(json_encode(["error" => "Connection failed: " . $conn->connect_error]));
 }
-<<<<<<< HEAD:Photo/dane_photo.php
 $sql = "SELECT id, marka, model, zdjecia FROM cardle ORDER BY rand() LIMIT 1";
 $result = $conn->query($sql);
 $data = [];
@@ -17,28 +16,8 @@ if ($result->num_rows > 0) {
     $data = $result->fetch_assoc();
 } else {
     $data = ["error" => "0 results"];
-=======
-
-$sql = "SELECT marka, model, zdjecia FROM cardle ORDER BY rand() LIMIT 1"
-$result = $conn->query($sql);
-
-$input = [];
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $input[] = $row;
-    }
-} else {
-    $input = ["error" => "0 results"];
->>>>>>> 82b92173550c945612c1b31f56acc20126aa38f8:Photo/api.php
 }
 
 echo json_encode($data);
 $conn->close();
-<<<<<<< HEAD:Photo/dane_photo.php
-=======
-
-header('Content-Type: application/json');
-echo json_encode($input);
->>>>>>> 82b92173550c945612c1b31f56acc20126aa38f8:Photo/api.php
 ?>
--- Photo/photo.js
+++ Photo/photo.js
@@ -3,8 +3,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const response = await fetch("api.php");
     const carData = await response.json();
-    console.log(response)
-    // console.log(carData)
+    console.log(carData)
 });
 
 //niech sie uleży (odbiera dane z pliku php)
@@ -13,30 +12,25 @@ function clearInput(){
     document.getElementById("guess").innerText = ""
 }
 
-function func(){
-    console.log("test")
-    document.getElementById("guess").value = ""
-}
-
 function checkEnter(event){
     if (event.key === "Enter"){
         document.getElementById("guess").click();
     }
 }
 
-fetch('dane_photo.php')
-.then(response => {
-    if (!response.ok){
-        throw new Error('Wystapil blad');
-    }
-    return response.json();
-})
-.then(data => {
-    console.log("id:", data.id);
-    console.log("zdj: ", data.zdjecia);
-    console.log("marka: ", data.marka);
-    console.log("model: ", data.model)
-})
-.catch(error =>{
-    console.error("wystapil blad", error);
-})
+// fetch('dane_photo.php')
+// .then(response => {
+//     if (!response.ok){
+//         throw new Error('Wystapil blad');
+//     }
+//     return response.json();
+// })
+// .then(data => {
+//     console.log("id:", data.id);
+//     console.log("zdj: ", data.zdjecia);
+//     console.log("marka: ", data.marka);
+//     console.log("model: ", data.model)
+// })
+// .catch(error =>{
+//     console.error("wystapil blad", error);
+// })
